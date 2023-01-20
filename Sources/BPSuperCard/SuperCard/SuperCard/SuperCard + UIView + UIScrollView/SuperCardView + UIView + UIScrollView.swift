8d1062cdbf12e3889ca327c00db83284fa5b5067
Savva <savva.shuliatev@iss.digital>
2023-01-20 17:44:06 +0300

    Add init with some contentView with listening another scrollView

---
+++ Sources/BPSuperCard/SuperCard/SuperCard/SuperCard + UIView + UIScrollView/SuperCardView + UIView + UIScrollView.swift
@@ -0,0 +1,48 @@
+//
+//  SuperCardView + UIView + UIScrollView.swift
+//
+//  Created by Савва Шулятьев on 20.01.2023.
+//
+
+import UIKit
+
+public extension SuperCardView {
+
+  /// It is compatible with any type of `UIScrollView`.
+  /// `scrollView.delegate` will be used by `ScrollDrawerViewContent`.
+  convenience init(
+    contentView: UIView,
+    scrollView: UIScrollView,
+    headerView: UIView
+  ) {
+    self.init(
+      content: ViewWithScrollViewCardViewContent(
+        contentView: contentView,
+        scrollView: scrollView
+      ),
+      headerView: headerView
+    )
+  }
+
+}
+
+public extension SuperSnappingView {
+
+  /// It is compatible with any type of `UIScrollView`.
+  /// `scrollView.delegate` will be used by `ScrollDrawerViewContent`.
+  convenience init(
+    contentView: UIView,
+    scrollView: UIScrollView,
+    headerView: UIView
+  ) {
+    self.init(
+      content: ViewWithScrollViewCardViewContent(
+        contentView: contentView,
+        scrollView: scrollView
+      ),
+      headerView: headerView
+    )
+  }
+
+}
+
